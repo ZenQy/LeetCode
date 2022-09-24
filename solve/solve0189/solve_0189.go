afef39c9834ff56ee8f562c07a398b0b784c1bca
ZenQy <zenqy.qin@gmail.com>
2022-09-24 20:25:23 +0800

    update

--- solve/solve0189/solve_0189.go
+++ solve/solve0189/solve_0189.go
@@ -2,7 +2,7 @@ package solve0189
 
 /**
  * @index 189
- * @title 旋转数组
+ * @title 轮转数组
  * @difficulty 中等
  * @tags array,math,two-pointers
  * @draft false
@@ -10,6 +10,6 @@ package solve0189
  * @frontendId 189
  */
 
-func rotate(nums []int, k int) {
+func rotate(nums []int, k int) []int {
 
 }
--- solve/solve0189/solve_0189_test.go
+++ solve/solve0189/solve_0189_test.go
@@ -1,2 +1,12 @@
 package solve0189
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRotate(t *testing.T) {
+	assert.Equal(t, rotate([]int{1, 2, 3, 4, 5, 6, 7}, 3), []int{5, 6, 7, 1, 2, 3, 4})
+	assert.Equal(t, rotate([]int{-1, -100, 3, 99}, 2), []int{3, 99, -1, -100})
+}
